45668a105eb167227caa945285e08f3285e11192
Chaoyue <chaoyue.zhao.2011@gmail.com>
2019-02-21 19:12:05 -0500

    finally fixed the api call issue with preventdefault behavior of the form

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const app = {};
 
 app.apiKey = "dtc2mjHS_wI_L9lXunSWrdSxvdQDreedErclvbJMvbUDoh2BCH4cif1qnQLjCZUfaYEsZgGWxcIH1z5bjb6QWQiZkoP0puCB5zB9J12Txrnhx3hYg1VYuyHs1GttXHYx";
-app.proxyUrl = "http://proxy.hackeryou.com/";
+app.proxyUrl = "https://cors-anywhere.herokuapp.com/";
 app.apiUrl = "https://api.yelp.com/v3/businesses/search?location=483queenstw";
 
 //CACHE DA SELECTORS
@@ -21,7 +21,8 @@ app.init = () => {
 //ADD EVENT LISTENER - ON FORM SUBMIT - RUN RENDER FUNCTION
 app.handleFormSubmit = () => {
 
-  $(".form").on("submit",function (){
+  $(".form").on("submit",function (e){
+    e.preventDefault();
     app.gatherUserInput();
   })
 };
@@ -33,43 +34,43 @@ app.gatherUserInput = () => {
   app.$limit = $("#limit").val();
   // app.$open = $("#open_now")
   app.$sort = $("#sort_by").val();
-  console.log(app.$sort);
   app.$categories = $("#categories").val();
 
-  app.getDataFromApi(app.$radius, app.$limit, app.$price, app.$sort, app.$categories);
 
+  // console.log(app.$sort);
+  app.getDataFromApi(app.$radius, app.$limit, app.$price, app.$sort, app.$categories);
+  // app.getDataFromApi(1000, 5, 2, "distance", "thai");
 }
 
 //CREATE METHOD TO GATHER DATA FROM FUSION API USING AJAX PULL WITH USER INPUT
-app.getDataFromApi = (radius,limit,price,sort,category) => {
+app.getDataFromApi = (radius, limit, price, sort, category) => {
   $.ajax({
-    url: app.proxyUrl,
+    url: app.proxyUrl + app.apiUrl,
     method: "GET",
-    dataType: "json",
+    headers: {
+      Authorization: `Bearer ${app.apiKey}`
+    },
     data: {
-      reqUrl: app.apiUrl,
-      params: {
-        radius: radius,
-        limit: limit,
-        price: price,
-        sort_by: sort,
-        categories: category
-      },
-      proxyHeaders: {
-        Authorization: `Bearer ${app.apiKey}`
-      }
+      radius: radius,
+      limit: limit,
+      price: price,
+      sort_by: sort,
+      categories: category
     }
-  }).then((result) => {
-     app.renderDOM(result.businesses);
-  }).fail((error) => {
-    
-    console.log('Server fucked up', error);
   })
+    .then(result => {
+      console.log(result);
+      app.renderDOM(result.businesses);
+    })
+    .fail(error => {
+      console.log("Server fucked up", error);
+    });
 }
 
 //CREATE METHOD TO RENDER DATA FROM API RESULTS TO DOM
 
 app.renderDOM = function(restaurants) {
+  console.log("rendering!")
   restaurants.map(function (restaurant) {
     $(".results").append(
       `${restaurant.name}`
